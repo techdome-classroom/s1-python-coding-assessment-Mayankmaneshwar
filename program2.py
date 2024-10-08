9a7a4ddae84e97f5f6f50b5523f3b685e4a5ab1d
Mayank Maneshwar <153177386+Mayankmaneshwar@users.noreply.github.com>
2024-10-08 12:46:05 +0530

    assessment completed program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,30 @@
-def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
-        return False
+def decode_message(s: str, p: str) -> bool:
+   
+    memo = {}
+    
+    def dfs(i, j):
+       
+        if i == len(s) and j == len(p):
+            return True
+       
+        if j == len(p):
+            return False
+        
+        if (i, j) in memo:
+            return memo[(i, j)]
+        
+       match = i < len(s) and (p[j] == s[i] or p[j] == '?')
+        
+         if p[j] == '*':
+          
+            memo[(i, j)] = dfs(i, j + 1) or (i < len(s) and dfs(i + 1, j))
+            return memo[(i, j)]
+        
+        if match:
+            memo[(i, j)] = dfs(i + 1, j + 1)
+            return memo[(i, j)]
+        
+         memo[(i, j)] = False
+         return False
+    
+    return dfs(0, 0)
